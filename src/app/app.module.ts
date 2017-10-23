d80c6ed7f2b8155d3ebdb0f72e1ec256cf6e2fb6
Yuri Calabrez <yuri.calabrez@gmail.com>
2017-10-23 12:21:29 -0200

    Implementando o carrinho de compras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,10 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
+
 import {RestaurantService} from "./restaurants/restaurant/resturant.service";
+import {ShoppingCartService} from "./restaurant-detail/shopping-cart/shopping-cart.service";
+
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
@@ -38,7 +41,7 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantService],
+  providers: [RestaurantService, ShoppingCartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
