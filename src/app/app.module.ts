404cc8071f4f62def91ded04eb370f5f5b2d40f3
Yuri Calabrez <yuri.calabrez@gmail.com>
2017-10-27 12:30:58 -0200

    Criado componente de rating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import { RadioComponent } from './shared/radio/radio.component';
 import { OrderItemsComponent } from './order/order-items/order-items.component';
 import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { RatingComponent } from './shared/rating/rating.component';
 
 
 @NgModule({
@@ -48,7 +49,8 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
   RadioComponent,
   OrderItemsComponent,
   DeliveryCostsComponent,
-  OrderSummaryComponent
+  OrderSummaryComponent,
+  RatingComponent
   ],
   imports: [
   BrowserModule,
