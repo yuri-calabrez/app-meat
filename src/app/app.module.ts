43426e913e7e84ee938d886f7a19ba72274a1e9c
Yuri Calabrez <yuri.calabrez@gmail.com>
2017-11-06 11:23:50 -0200

    Modificando a estratégia de navegação(hash)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {NgModule, LOCALE_ID} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {RouterModule, PreloadAllModules} from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HashLocationStrategy, LocationStrategy} from "@angular/common";
 
 import {ROUTES} from './app.routes';
 
@@ -20,6 +21,7 @@ import {ReviewsComponent} from './restaurant-detail/reviews/reviews.component';
 import {OrderSummaryComponent} from './order-summary/order-summary.component';
 
 import {SharedModule} from "./shared/shared.module";
+import { NotFoundComponent } from './not-found/not-found.component';
 //import {CoreModule} from "./core/core.module";
 
 @NgModule({
@@ -34,7 +36,8 @@ import {SharedModule} from "./shared/shared.module";
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderSummaryComponent
+    OrderSummaryComponent,
+    NotFoundComponent
     ],
     imports: [
     BrowserModule,
@@ -44,7 +47,7 @@ import {SharedModule} from "./shared/shared.module";
     //CoreModule,
     RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
     ],
-    providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
+    providers: [{provide: LocationStrategy, useClass: HashLocationStrategy},{provide: LOCALE_ID, useValue: 'pt-BR'}],
     bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,20 +5,22 @@ import {RestaurantDetailComponent} from "./restaurant-detail/restaurant-detail.c
 import {MenuComponent} from "./restaurant-detail/menu/menu.component";
 import {ReviewsComponent} from "./restaurant-detail/reviews/reviews.component";
 import {OrderSummaryComponent} from "./order-summary/order-summary.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
 export const ROUTES: Routes = [
-{path: '', component: HomeComponent},
-{path: 'restaurants', component: RestaurantsComponent},
-{
-    path: 'restaurants/:id',
-    component: RestaurantDetailComponent,
-    children: [
-    {path: '', redirectTo: 'menu', pathMatch: 'full'},
-    {path: 'menu', component: MenuComponent},
-    {path: 'reviews', component: ReviewsComponent}
-    ]
-},
-{path: 'order', loadChildren: './order/order.module#OrderModule'},
-{path: 'order-summary', component: OrderSummaryComponent},
-{path: 'about', loadChildren: './about/about.module#AboutModule'}
+    {path: '', component: HomeComponent},
+    {path: 'restaurants', component: RestaurantsComponent},
+    {
+        path: 'restaurants/:id',
+        component: RestaurantDetailComponent,
+        children: [
+            {path: '', redirectTo: 'menu', pathMatch: 'full'},
+            {path: 'menu', component: MenuComponent},
+            {path: 'reviews', component: ReviewsComponent}
+        ]
+    },
+    {path: 'order', loadChildren: './order/order.module#OrderModule'},
+    {path: 'order-summary', component: OrderSummaryComponent},
+    {path: 'about', loadChildren: './about/about.module#AboutModule'},
+    {path: '**', component: NotFoundComponent}
 ];
