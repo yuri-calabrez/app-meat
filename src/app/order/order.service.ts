332772f82051e29e88541a6e2074e8d80d71da8e
Yuri Calabrez <yuri.calabrez@gmail.com>
2017-11-14 12:19:47 -0200

    Realizando compra com autenticação

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
@@ -7,11 +7,14 @@ import {ShoppingCartService} from '../restaurant-detail/shopping-cart/shopping-c
 import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model';
 import {Order, OrderItem} from './order.model';
 import {MEAT_API} from '../app.api';
+import {LoginService} from '../security/login/login.service';
 
 
 @Injectable()
 export class OrderService {
-	constructor(private cartService: ShoppingCartService, private http: HttpClient){}
+	constructor(private cartService: ShoppingCartService,
+		private http: HttpClient,
+		private loginService: LoginService){}
 
 	cartItems(): CartItem[] {
 		return this.cartService.items;
@@ -34,7 +37,11 @@ export class OrderService {
 	}
 
 	checkOrder(order: Order): Observable<string> {
-		return this.http.post<Order>(`${MEAT_API}/orders`, order)
+		let headers = new HttpHeaders();
+		if(this.loginService.isLoggedIn()) {
+			headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`);
+		}
+		return this.http.post<Order>(`${MEAT_API}/orders`, order, {headers})
 		.map(order => order.id);
 	}
 
--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'; 
 import {LoginService} from './login.service';
 import {User} from './user.model';
+import {NotificationService} from '../../shared/messages/notification.service';
 
 @Component({
 	selector: 'mt-login',
@@ -12,7 +13,9 @@ export class LoginComponent implements OnInit {
 
 	loginForm: FormGroup
 
-	constructor(private fb: FormBuilder, private loginService: LoginService) { }
+	constructor(private fb: FormBuilder, 
+		private loginService: LoginService,
+		private notificationService: NotificationService) { }
 
 	ngOnInit() {
 		this.loginForm = this.fb.group({
@@ -24,7 +27,10 @@ export class LoginComponent implements OnInit {
 	login(){
 		this.loginService.login(this.loginForm.value.email, 
 			this.loginForm.value.password)
-		.subscribe(user => console.log(user));
+		.subscribe(user =>
+			this.notificationService.notify(`Bem vindo ${user.name}`),
+			response => //HttpErrorResponse
+			this.notificationService.notify(response.error.message));
 	}
 
 
