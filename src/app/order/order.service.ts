b49d3e20603af337aa6a717ec30c7adfc74b2566
Yuri Calabrez <yuri.calabrez@gmail.com>
2017-12-14 10:53:55 -0200

    Utilizando HTTP Interceptors

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -7,14 +7,12 @@ import {ShoppingCartService} from '../restaurant-detail/shopping-cart/shopping-c
 import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model';
 import {Order, OrderItem} from './order.model';
 import {MEAT_API} from '../app.api';
-import {LoginService} from '../security/login/login.service';
 
 
 @Injectable()
 export class OrderService {
 	constructor(private cartService: ShoppingCartService,
-		private http: HttpClient,
-		private loginService: LoginService){}
+		private http: HttpClient){}
 
 	cartItems(): CartItem[] {
 		return this.cartService.items;
@@ -37,11 +35,7 @@ export class OrderService {
 	}
 
 	checkOrder(order: Order): Observable<string> {
-		let headers = new HttpHeaders();
-		if(this.loginService.isLoggedIn()) {
-			headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`);
-		}
-		return this.http.post<Order>(`${MEAT_API}/orders`, order, {headers})
+		return this.http.post<Order>(`${MEAT_API}/orders`, order)
 		.map(order => order.id);
 	}
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 import {NgModule, ModuleWithProviders} from "@angular/core";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+
 import {CommonModule} from "@angular/common";
 import {InputComponent} from "./input/input.component";
 import {RadioComponent} from "./radio/radio.component";
@@ -14,6 +16,7 @@ import {NotificationService} from './messages/notification.service';
 import {LoginService} from '../security/login/login.service';
 import {LoggedInGuard} from '../security/loggedin.guard';
 import {LeaveOrderGuard} from '../order/leave-order.guard';
+import {AuthInterceptor} from '../security/auth.interceptor';
 
 @NgModule({
     declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
@@ -34,7 +37,8 @@ export class SharedModule {
             NotificationService,
             LoginService,
             LoggedInGuard,
-            LeaveOrderGuard
+            LeaveOrderGuard,
+            {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
             ]
         }
     }
